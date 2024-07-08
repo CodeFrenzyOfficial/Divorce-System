6f79c33c6ee55ced07b4aad8f5fc873643c97f95
maaziqbal08 <maaziqbal631@gmail.com>
2024-07-08 21:25:06 +0500

    make SSR false for state components

--- app/state/page.js
+++ app/state/page.js
@@ -1,31 +1,110 @@
+import dynamic from "next/dynamic";
 import React from "react";
-import TitleSection from "../components/States/TitleSection";
-import ArrowSection from "../components/States/ArrowSection";
-import DisclaimerSection from "../components/States/DisclaimerSection";
-import OnlineDivorceSection from "../components/States/OnlineDivorceSection";
-import FilingSection from "../components/States/FilingSection";
-import ResidencySection from "../components/States/ResidencySection";
-import CommitmentSection from "../components/States/CommitmentSection";
-import Banner1 from "../components/States/Banner1";
-import ValidGround from "../components/States/ValidGround";
-import Banner2 from "../components/States/Banner2";
-import Custody from "../components/States/Custody";
-import ChildSupportRules from "../components/States/ChildSupport";
-import Banner3 from "../components/States/Banner3";
-import SpousalSupport from "../components/States/SpousalSupport";
-import PropertyDivision from "../components/States/PropertyDivision";
-import WhyChooseUs from "../components/States/WhyUs";
-import MediationSupport from "../components/States/MediationSupport";
-import DivorceForms from "../components/States/DivorceForms";
-import Banner4 from "../components/States/Banner4";
-import FilingFee from "../components/States/FilingFee";
-import HowLong from "../components/States/HowLong";
-import FaqAccordion from "../components/States/Faq";
+
+const TileSection = dynamic(() => import("../components/States/TitleSection"), {
+  ssr: false,
+});
+const ArrowSection = dynamic(
+  () => import("../components/States/ArrowSection"),
+  {
+    ssr: false,
+  }
+);
+const DisclaimerSection = dynamic(
+  () => import("../components/States/DisclaimerSection"),
+  {
+    ssr: false,
+  }
+);
+const OnlineDivorceSection = dynamic(
+  () => import("../components/States/OnlineDivorceSection"),
+  {
+    ssr: false,
+  }
+);
+const FilingSection = dynamic(
+  () => import("../components/States/FilingSection"),
+  {
+    ssr: false,
+  }
+);
+const ResidencySection = dynamic(
+  () => import("../components/States/ResidencySection"),
+  {
+    ssr: false,
+  }
+);
+const CommitmentSection = dynamic(
+  () => import("../components/States/CommitmentSection"),
+  {
+    ssr: false,
+  }
+);
+const Banner1 = dynamic(() => import("../components/States/Banner1"), {
+  ssr: false,
+});
+const ValidGround = dynamic(() => import("../components/States/ValidGround"), {
+  ssr: false,
+});
+const Banner2 = dynamic(() => import("../components/States/Banner2"), {
+  ssr: false,
+});
+const Custody = dynamic(() => import("../components/States/Custody"), {
+  ssr: false,
+});
+const ChildSupportRules = dynamic(
+  () => import("../components/States/ChildSupport"),
+  {
+    ssr: false,
+  }
+);
+const Banner3 = dynamic(() => import("../components/States/Banner3"), {
+  ssr: false,
+});
+const SpousalSupport = dynamic(
+  () => import("../components/States/SpousalSupport"),
+  {
+    ssr: false,
+  }
+);
+const PropertyDivision = dynamic(
+  () => import("../components/States/PropertyDivision"),
+  {
+    ssr: false,
+  }
+);
+const WhyChooseUs = dynamic(() => import("../components/States/WhyUs"), {
+  ssr: false,
+});
+const MediationSupport = dynamic(
+  () => import("../components/States/MediationSupport"),
+  {
+    ssr: false,
+  }
+);
+const DivorceForms = dynamic(
+  () => import("../components/States/DivorceForms"),
+  {
+    ssr: false,
+  }
+);
+const Banner4 = dynamic(() => import("../components/States/Banner4"), {
+  ssr: false,
+});
+const FilingFee = dynamic(() => import("../components/States/FilingFee"), {
+  ssr: false,
+});
+const HowLong = dynamic(() => import("../components/States/HowLong"), {
+  ssr: false,
+});
+const FaqAccordion = dynamic(() => import("../components/States/Faq"), {
+  ssr: false,
+});
 
 const State = () => {
   return (
     <div className="flex flex-col gap-5  py-5">
-      <TitleSection />
+      <TileSection />
       <ArrowSection />
       <DisclaimerSection />
       <OnlineDivorceSection />
